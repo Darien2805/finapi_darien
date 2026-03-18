62e566c35648ce4ed133fd22a6e9c11f3f018e0a
TEK Student <TEKstudent@vlab.lan>
2026-03-18 00:35:47 -0700

    new service

--- tests/smoke_tests.py
+++ tests/smoke_tests.py
@@ -1,6 +1,6 @@
 import requests
 
-BASE_URL = "finapi-darien-dbgrepepayd3geeb.eastus-01.azurewebsites.net" # <------ Replace with your actual base URL
+BASE_URL = "https://finapi-darien-dbgrepepayd3geeb.eastus-01.azurewebsites.net/" # <------ Replace with your actual base URL
 
 def test_home():
     r = requests.get(f"{BASE_URL}/")
